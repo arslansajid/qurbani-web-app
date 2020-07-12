1c238c9939ae5854f1cf337362ea9e1f560ed293
Arslan Sajid <arslansajid951@gmail.com>
2020-07-12 16:50:45 +0500

    Added dialog component

---
+++ src/components/Dialog/Dialog.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const DialogForm = ({
+    title,
+    open,
+    message,
+    applyForm,
+    cancelForm,
+    hideActions,
+}) => {
+    return (
+        <div>
+            <Dialog open={open} onClose={cancelForm} fullWidth={true} maxWidth={'sm'} disableBackdropClick>
+                <DialogTitle id='form-dialog-title'>{title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText component={'div'}>{message}</DialogContentText>
+                </DialogContent>
+                {hideActions ? null : (
+                    <>
+                        <DialogActions>
+                            {
+                                applyForm && (
+                                    <Button onClick={applyForm} color='primary' type='submit'>
+                                        Save
+                                    </Button>
+                                )
+                            }
+                            {
+                                cancelForm && (
+                                    <Button onClick={cancelForm} color='secondary' type='submit'>
+                                        Cancel
+                                    </Button>
+                                )
+                            }
+                        </DialogActions>
+                    </>
+                )}
+            </Dialog>
+        </div>
+    );
+};
+
+export default DialogForm;
